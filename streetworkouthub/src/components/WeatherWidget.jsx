933baff1df7c4739388fdd8f839dc7c1fdc2723a
dragon <basselt@abv.bg>
2025-12-05 01:10:42 +0200

    editing weather dynamic

--- streetworkouthub/src/components/WeatherWidget.jsx
+++ streetworkouthub/src/components/WeatherWidget.jsx
@@ -12,10 +12,24 @@ export default function WeatherWidget() {
   if (!weather) return <p className="text-white">Loading weather...</p>;
 
   return (
-    <div className="p-3 border border-danger rounded" style={{ width: "220px" }}>
+    <div className="p-3 border border-danger rounded" style={{ width: "280px" }}>
+      
+      <small className="text-secondary">
+        Feels sporty today 💪
+        <br />
+        let's check the weather
+      </small>
       <h6 className="text-danger">Sofia Weather</h6>
       <p className="m-0">{weather.temperature}°C</p>
-      <small className="text-secondary">Feels sporty today 💪</small>
+
+      <small className="text-secondary">
+        {weather.temperature > 30
+          ? "Too hot for sports today 🌡️"
+          : weather.temperature < 10
+          ? "Too cold for sports today ❄️"
+          : "Good weather for sports today ☀️"}
+      </small>
+
     </div>
   );
 }
