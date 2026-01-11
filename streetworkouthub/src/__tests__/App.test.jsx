c0ea98a559fdc7f3e905efdb8dc176d457f54be5
dragon <basselt@abv.bg>
2026-01-11 17:43:54 +0200

    updating tests and yml

--- streetworkouthub/src/__tests__/App.test.jsx
+++ streetworkouthub/src/__tests__/App.test.jsx
@@ -2,12 +2,17 @@ import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
 import App from '../App'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import  store  from "../store/store";
+
 
 test('renders navigation brand name', () => {
   render(
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   )
 
 expect(screen.getByRole('link', { name: /StreetWorkoutHub/i })).toBeInTheDocument();
--- streetworkouthub/src/__tests__/Catalog.test.jsx
+++ streetworkouthub/src/__tests__/Catalog.test.jsx
@@ -1,7 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom'; // <-- add this
+import '@testing-library/jest-dom';
 import Catalog from "../pages/Catalog";
 import * as postService from "../services/postService";
+import { Provider } from 'react-redux'
+import  store  from "../store/store";
 
 // test("Catalog shows empty state message", () => {
 //   render(<Catalog />);
@@ -13,6 +15,10 @@ vi.mock("../services/postService");
 
 test("shows empty state when no posts", async () => {
   postService.getAll.mockResolvedValue([]);
-  render(<Catalog />);
+  render(
+    <Provider store={store}>
+      <Catalog />
+    </Provider>
+    );
   expect(await screen.findByText("No posts yet!")).toBeInTheDocument();
 });
